a86a4ff0b125c263d5d55dfe48997f37695e5f88
Dilthey Aislan Lopes de Paula <diltheyaislan@gmail.com>
2022-06-10 16:53:26 +0100

    add base entity class

---
+++ src/main/java/com/diltheyaislan/moviesbattle/api/domain/entity/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.diltheyaislan.moviesbattle.api.domain.entity;
+
+import java.io.Serializable;
+
+public abstract class BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+}
