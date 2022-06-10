7fe629f659907b68001697959b718f67f6b42afd
Dilthey Aislan Lopes de Paula <diltheyaislan@gmail.com>
2022-06-10 16:53:38 +0100

    add user entity

---
+++ src/main/java/com/diltheyaislan/moviesbattle/api/domain/entity/User.java
@@ -0,0 +1,66 @@
+package com.diltheyaislan.moviesbattle.api.domain.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+public class User extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(
+	    name = "UUID",
+	    strategy = "org.hibernate.id.UUIDGenerator"
+	)
+	@Column(name = "id", updatable = false, nullable = false)
+	@ColumnDefault("random_uuid()")
+	@Type(type = "uuid-char")
+	private UUID id;
+	
+	@Column
+	@Size(max = 100)
+	private String name;
+	
+	@Column
+	@Size(max = 40)
+	private String username;
+	
+	@Column
+	@Size(max = 30)
+	private String password;
+	
+	@Column(updatable = false, nullable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+}
